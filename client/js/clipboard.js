a3df4a906b6a3c4d25a9eca452b54a8a7a7db3ae
Alistair McKinlay <github@10people.co.uk>
2017-06-14 15:00:58 +0100

    Add < > around usernames when copying lines

--- client/js/clipboard.js
+++ client/js/clipboard.js
@@ -10,6 +10,9 @@ function addLink() {
 	copytext = copytext.replace(/\n(\d{2}:\d{2}(:\d{2})?)/g, '<br>$1'); // Replace first newlines on each message with <br>
 	copytext = copytext.replace(/\n/g, ' '); // Remove all other newlines
 
+  // Add < and > around username
+	copytext = copytext.replace(/(<br>\d{2}:\d{2}(:\d{2})?) ([^ ]*) /g, '$1 <$3> ');
+
 	// hide the newly created container
 	newdiv.style.position = "absolute";
 	newdiv.style.left = "-99999px";
